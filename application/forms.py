7820239529f19e4ab8da733ee0b2d1bdf5c199cb
Tobi Jackson <jtsproduction@live.com>
2020-07-31 14:42:24 +0100

    completed routes

---
+++ application/forms.py
@@ -0,0 +1,105 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, DecimalField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Places, Rounding
+from application.models import Users, Tanks, Tests
+from flask_login import current_user
+
+##### register form #################
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=30)])
+
+    last_name = StringField('Last Name',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=30)])
+
+    email = StringField('Email',
+        validators = [
+            DataRequired(),
+            Email()])
+
+    submit = SubmitField('Create Account')
+
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+
+        if user:
+            raise ValidationError('Email already in use')
+
+############ register tanks ##############
+
+class RegistrationTankForm(FlaskForm):
+    name = StringField('Name',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=30)])
+
+    description = StringField('Decription',
+        validators = [
+            DataRequired(),
+            Length(min=0, max=100)])
+
+    submit = SubmitField('Create Tank')
+
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+
+        if user:
+            raise ValidationError('Email already in use')
+
+############### submit tests ########################
+class TestsForm(FlaskForm):
+    ammonia = DecimalField('Ammonia',
+        Places=2, Rounding=None,
+        validators = DataRequired())
+
+    nitrate = DecimalField('Nitrate',
+        Places=2, Rounding=None,
+        validators = DataRequired())
+
+    nitrite = DecimalField('Nitrite',
+        Places=2, Rounding=None,
+        validators = DataRequired())
+
+    submit = SubmitField('Create Tank')
+
+################ login form ##################
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+        validators=[
+            DataRequired(),
+            Email()])
+
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+################# update account #########################
+
+class UpdateAccountForm(FlaskForm):
+    first_name = StringField('First Name',
+        validators=[
+            DataRequired(),
+            Length(min=4, max=20)
+        ])
+    last_name = StringField('Last Name',
+        validators=[
+            DataRequired(),
+            Length(min=4, max=25)
+        ])
+    email = StringField('Email',
+        validators=[
+            DataRequired(),
+            Email()
+        ])
+    submit = SubmitField('Update')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = Users.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already in use')
--- application/models.py
+++ application/models.py
@@ -8,6 +8,7 @@ class Users(db.Model):
     id = db.Column(db.Interger, primary_key=True, nullable=False, unique=True)
     first_name = db.Column(db.String(20), nullable=False)
     last_name = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
 
     def __repr__(self):
         return ''.join([
