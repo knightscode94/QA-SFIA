e434bf3559253071ce1f3a23218fd060389b6ea3
Tobi Jackson <jtsproduction@live.com>
2020-08-03 18:28:30 +0100

    validation

--- application/forms.py
+++ application/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, DecimalField, SelectField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Name
 from wtforms_sqlalchemy.fields import QuerySelectField
 from application.models import Users, Tanks, Tests
 from flask_login import current_user
