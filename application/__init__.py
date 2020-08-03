53993d7c9374fe2ec175b6e6be0c68a3d0533ee8
Tobi Jackson <jtsproduction@live.com>
2020-08-03 07:25:47 +0100

    formatting

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,5 @@
 ## import flask, sql
+from application import routes
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -12,6 +13,3 @@ app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-
-#### application routes ####
-from application import routes
