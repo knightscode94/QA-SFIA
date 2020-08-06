bc2dd1952fb431768db7311e6d602e9dd765ad88
Tobi Jackson <jtsproduction@live.com>
2020-08-06 17:46:04 +0100

    first two tests

--- tests/test_int.py
+++ tests/test_int.py
@@ -66,19 +66,6 @@ class TestLogin(TestBase):
     def test_login(self):
         self.driver.find_element_by_xpath("/html/body/a[3]").click()
         time.sleep(1)
-
-        # Fill in registration form
-        self.driver.find_element_by_xpath('//*[@id="email"]').send_keys(test_admin_email)
-        self.driver.find_element_by_xpath('//*[@id="first_name"]').send_keys(
-            test_admin_first_name)
-        self.driver.find_element_by_xpath('//*[@id="last_name"]').send_keys(
-            test_admin_last_name)
-        self.driver.find_element_by_xpath('//*[@id="submit"]').click()
-        time.sleep(1)
-
-        self.driver.find_element_by_xpath('/html/body/a[4]').click()
-        time.sleep(1)
-
         self.driver.find_element_by_xpath('//*[@id="email"]').send_keys(test_admin_email)
         self.driver.find_element_by_xpath('//*[@id="submit"]').click()        
         time.sleep(2)
