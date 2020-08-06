84ee32263cd7bd9614c05ebede7357a5eed2ba7c
Tobi Jackson <jtsproduction@live.com>
2020-08-06 13:37:35 +0100

    test submit

--- application/models.py
+++ application/models.py
@@ -21,7 +21,6 @@ class Tanks(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
     name = db.Column(db.String(25), nullable=True, unique=True)
     description = db.Column(db.String(100), nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     ammonia = db.Column(db.Float, nullable=False)
     nitrate = db.Column(db.Float, nullable=False)
