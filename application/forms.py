91a432ba876a37f64932470963e999cf02f2abf0
Tobi Jackson <jtsproduction@live.com>
2020-08-06 11:51:32 +0100

    models error

--- application/forms.py
+++ application/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, DecimalField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from wtforms_sqlalchemy.fields import QuerySelectField
-from application.models import Users, Tests, Tanks
+from application.models import Users
 from flask_login import current_user
 
 ##### register form #################
