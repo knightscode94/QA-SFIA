27a9f6e5f255fbcdacf39a77f905de062b397703
Tobi Jackson <jtsproduction@live.com>
2020-08-07 09:43:08 +0100

    first two tests

--- application/routes.py
+++ application/routes.py
@@ -42,12 +42,14 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
-         login_user(user)
-          next_page = request.args.get('next')
-           if next_page:
-                return redirect(next_page)
-            else:
-                return redirect(url_for('home'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if next_page:
+            return redirect(next_page)
+        else:
+            return redirect(url_for('home'))
+    else:
+        return redirect(url_for('register'))
     return render_template('login.html', title='Login', form=form)
 
 ### update account ####
